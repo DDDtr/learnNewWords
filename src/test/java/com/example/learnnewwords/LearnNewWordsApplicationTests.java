c7fbd6a17b22db05978decb76fcab03ccd2dfee9
User Name <user@email.com>
2022-12-27 16:44:47 +0900

    first commit

---
+++ src/test/java/com/example/learnnewwords/LearnNewWordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnnewwords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnNewWordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
